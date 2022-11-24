443fc7cea4c4d533d1ba2805462dd83428cd5fe9
mathieuneyret <neyret.mathieu69@gmail.com>
2022-11-24 12:24:08 +0100

    Speech recognition for voice

--- src/components/PhoneArabic.vue
+++ src/components/PhoneArabic.vue
@@ -3,10 +3,45 @@ import { ref } from "vue";
 
 const text = ref("");
 
+/*const listWords = [
+  "Astrologie",
+  "Manteau",
+  "Téléphone",
+  "Ordinateur",
+  "Chargeur",
+  "Voiture",
+  "Lune",
+  "Planète",
+  "Lunettes"
+];
+
+function playSpeechRandom() {
+  const randomWords = listWords[Math.floor(Math.random()*listWords.length)];
+  const speechRandom = new SpeechSynthesisUtterance(randomWords);
+  speechSynthesis.speak(speechRandom);
+} */
+
 function playSpeechSynthesis() {
-  let utterance = new SpeechSynthesisUtterance(text.value);
+  const utterance = new SpeechSynthesisUtterance(text.value);
   speechSynthesis.speak(utterance);
+
+  const recognition = new webkitSpeechRecognition();
+//recognition.continuous = false;
+  recognition.lang = 'fr-FR';
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+
+  recognition.start();
+
+  recognition.onresult = function(event) {
+    const resultEvent = event.results[0][0].transcript;
+    const divResult = document.getElementById("result-voice")!;
+    divResult.innerHTML = resultEvent;
+    recognition.stop();
+  }
+
 }
+
 
 </script>
 
@@ -16,6 +51,8 @@ function playSpeechSynthesis() {
     <input v-model="text" type="text" />
     <button type="submit">submit</button>
   </form>
+  <div id="result-voice"></div>
+  <!--<button type="submit" @click="playSpeechRandom">Mots aléatoire</button> -->
 </template>
 
 <style scoped>
